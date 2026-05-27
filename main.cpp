9fa62a02e6892604a2d6ae744fe3ce45be7cc45b
Ny Hasina <djazejhasi@gmail.com>
2026-05-27 14:10:34 +0300

    update: basic unit test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(void)
 {
 	Vector<float> v = {2.0, 0.0};
+	Matrix<float> m(3, 3, {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0});
 
 	std::cout << v << std::endl;
+	std::cout << m << std::endl;
 }
