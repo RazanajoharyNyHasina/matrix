1e44bd3d17e3cb3728a24149c6f0bfd3249a0b9b
Ny Hasina <djazejhasi@gmail.com>
2026-05-27 13:54:02 +0300

    update: basic implementation of Vector

---
+++ include/Vector.hpp
@@ -0,0 +1,65 @@
+#ifndef __VECTOR_HPP__
+#define __VECTOR_HPP__
+
+#include <vector>
+#include <cstddef>
+#include <iostream>
+
+template <typename K>
+class Vector
+{
+private:
+	std::vector<K> data;
+
+public:
+	Vector(std::initializer_list<K> values) : data(values) {}
+
+	Vector<K> &operator=(const Vector &other)
+	{
+		if (this != &other)
+			data = other.data;
+		return (*this);
+	}
+
+	size_t size(void) const
+	{
+		return data.size();
+	}
+
+	K &operator[](size_t index)
+	{
+		return data[index];
+	}
+
+	const K &operator[](size_t index) const
+	{
+		return data[index];
+	}
+
+	template <typename T>
+	friend std::ostream &operator<<(std::ostream &stream, const Vector<T> &vector);
+
+	~Vector(void) {};
+};
+
+template <typename K>
+std::ostream &operator<<(std::ostream &stream, const Vector<K> &vector)
+{
+	stream << "Vector: {";
+
+	for (typename std::vector<K>::const_iterator i = vector.data.begin();
+		i != vector.data.end();
+		++i)
+	{
+		stream << *i;
+
+		if (i != vector.data.end() - 1)
+			stream << ",";
+	}
+
+	stream << "}";
+
+	return stream;
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "./include/Vector.hpp"
+#include "./include/Matrix.hpp"
+#include <iostream>
+
+int main(void)
+{
+	Vector<float> v = {2.0, 0.0};
+
+	std::cout << v << std::endl;
+}
